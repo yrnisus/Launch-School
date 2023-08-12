9ee671cb01df66119861e9a9e3e1409439955b0e
yrnisus <codyjmckeefry@gmail.com>
2023-08-12 16:07:59 +0000

    Finished initial version

--- ruby/RB110/Lesson 3/Twenty-One/21.rb
+++ ruby/RB110/Lesson 3/Twenty-One/21.rb
@@ -1,33 +1,5 @@
 require 'pry'
 
-=begin
-You start with a normal 52-card deck consisting of the 4 suits
-(hearts, diamonds, clubs, and spades), and 13 values (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace).
-
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-
-=end
-
-
-=begin
-
-=end
-
-# CARDS_ARRAY = { '1': 1, '2': 2, '3': 3,
-#                 '4': 4, '5': 5, '6': 6,
-#                 "7": 7, "8": 8, '9': 9,
-#                 '10': 10, "Jack": 10, 'Queen': 10,
-#                 'King': 10, 'Ace': 11
-# }
-
 CARDS_ARRAY = (2..10).to_a + ["Jack", "Queen", "King", "Ace"]
 FACE_CARDS_VALUES = {
   "Jack"=> 10, "Queen"=> 10,
@@ -59,54 +31,133 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def display_hands(player, computer)
+def display_hands_start(player, dealer)
   display_player_hand(player)
-  prompt "Computer Hand - #{computer.sample}"
+  display_dealer_hand_hidden(dealer)
+end
+
+def display_hands(player, dealer)
+  display_player_hand(player)
+  display_dealer_hand_full(dealer)
+end
+
+def display_hand(hand)
+  msg = hand.map do |card|
+    "[#{card}] "
+  end.join
 end
 
 def display_player_hand(hand)
-  total = 0
-  msg = hand.map do |card|
-    if card.class == String
-      total += FACE_CARDS_VALUES[card]
-    else
-      total += card
-    end
-    "[#{card}] "
-  end.join
-  prompt "Player Hand: #{msg}"
-  prompt "#{total}"
+  prompt "Your cards: #{display_hand(hand)}"
+  prompt "#{count_cards(hand)}"
 end
 
-def player_turn(player_hand, deck)
+def display_dealer_hand_hidden(hand)
+  prompt "Dealer's cards:  [#{hand.first}] [?]"
+end
+
+def display_dealer_hand_full(hand)
+  prompt "Dealer's cards: #{display_hand(hand)}"
+  prompt "#{count_cards(hand)}"
+end
+
+def player_turn(player_hand, dealer_hand, deck)
   loop do
-    puts ""
     prompt "(H)it or (S)tay?"
     input = gets.chomp
     if input.downcase.start_with?('h')
       player_hand << deal_card(deck)
-      prompt "#{player_hand}"
+      display_player_hand(player_hand)
+    else
+      break
+    end
+
+    if count_cards(player_hand) > 21
+      prompt "Bust. You lose"
+      break
+    end
+    system 'clear'
+    display_hands_start(player_hand, dealer_hand)
+  end
+  # system 'clear'
+  # dealer_turn(dealer_hand, deck)
+  # display_hands(player_hand, dealer_hand)
+end
+
+def dealer_turn(hand, deck)
+  total = count_cards(hand)
+  loop do
+    if total < 17
+      hand << deal_card(deck)
+      total = count_cards(hand)
     else
       break
     end
   end
-  prompt "Your total is num"
+end
+
+def determine_winner(player_hand, dealer_hand)
+  player_total = count_cards(player_hand)
+  dealer_total = count_cards(dealer_hand)
+  display_hands(player_hand, dealer_hand)
+
+  if player_total > 21
+    prompt "Dealer wins!"
+    return nil
+  elsif dealer_total > 21
+     prompt "You win!"
+    return nil
+  end
+  # neither player busted
+  if player_total > dealer_total
+    prompt "You win!"
+  elsif player_total == dealer_total
+    prompt "It's a tie!"
+  else
+    prompt "Dealer wins!"
+  end
+
+end
+
+def count_cards(hand)
+    total = 0
+    hand.map do |card|
+      if card.class == String
+        total += FACE_CARDS_VALUES[card]
+      else
+        total += card
+      end
+    end
+  total = handle_ace(total, hand.count("Ace")) if hand.include?("Ace")
+  total
+end
+
+def handle_ace(total, aces)
+  loop do
+    if total > 21
+      total -= 10
+    end
+    aces -= 1
+    break if aces == 0
+  end
+  total
 end
 
 
 deck = initialize_deck
 player_hand = []
-computer_hand = []
+dealer_hand = []
 
 # gameplay start
 loop do
   loop do
     2.times { player_hand << deal_card(deck) }
-    2.times { computer_hand << deal_card(deck) }
+    2.times { dealer_hand << deal_card(deck) }
     break
   end
-  display_hands(player_hand, computer_hand)
-  player_turn(player_hand, deck)
-  p player_hand
+  display_hands_start(player_hand, dealer_hand)
+  player_turn(player_hand, dealer_hand, deck)
+  dealer_turn(dealer_hand, deck)
+  determine_winner(player_hand, dealer_hand)
 break
 end
